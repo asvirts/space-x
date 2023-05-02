3aaef13db4789d6005da7068de9f8ca0fc58ea46
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-05-01 20:04:11 -0500

    added ship query

--- space-x/src/Query.jsx
+++ space-x/src/Query.jsx
@@ -2,10 +2,17 @@ import { useQuery, gql } from "@apollo/client"
 
 const FILMS_QUERY = gql`
   {
-    launchesPast(limit: 10) {
+    launchesPast(limit: 25) {
       id
       mission_name
     }
+    ships(limit: 25) {
+        id
+        model
+        name
+        type
+        status
+      }
   }
 `
 
@@ -16,13 +23,23 @@ export default function Query() {
   if (error) return <pre>{error.message}</pre>
 
   return (
-    <div>
-      <h1>SpaceX Launches</h1>
-      <ol>
-        {data.launchesPast.map((launch) => (
-          <li key={launch.id}>{launch.mission_name}</li>
-        ))}
-      </ol>
+    <div className="query">
+        <div className="col">
+            <h2>SpaceX Launches</h2>
+            <ol>
+                {data.launchesPast.map((launch) => (
+                <li key={launch.id}>{launch.mission_name}</li>
+                ))}
+            </ol>
+        </div>
+        <div className="col">
+            <h2>SpaceX Ships</h2>
+            <ol>
+                {data.ships.map((ship) => (
+                <li key={ship.id}>{ship.name}</li>
+                ))}
+            </ol>
+        </div>
     </div>
   );
 }
